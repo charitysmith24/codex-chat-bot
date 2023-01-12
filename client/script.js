228ac35dfde980467b7fd1d80e36de09fdaed142
charitysmith24 <charity.smith24@gmail.com>
2023-01-12 17:49:04 -0500

    Added new host

--- client/script.js
+++ client/script.js
@@ -73,7 +73,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // fetch data returned from ai server -> bot response
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("https://codex-ai-r9bl.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
